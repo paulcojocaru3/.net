6b4e542caa1e7e86c9da9d8c69bdbd366ddadb4c
paulcojocaru3 <148447157+paulcojocaru3@users.noreply.github.com>
2025-10-06 11:38:38 +0300

    Add files via upload

---
+++ ex3.cs
@@ -0,0 +1,32 @@
+﻿ex3.t();
+
+
+
+public record student(string Name);
+
+public class ex3
+{
+    
+    public static void t()
+    {
+        List<student> listaDeNume = new List<student>();
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input == "end")
+                {
+                break;
+                }
+            else
+            {
+                listaDeNume.Add(new student(input));
+            }
+
+        }
+
+        foreach (var s in listaDeNume)
+        {
+            Console.WriteLine(s.Name);
+        }
+    }
+}
